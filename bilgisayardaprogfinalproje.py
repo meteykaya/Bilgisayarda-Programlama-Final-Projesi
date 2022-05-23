2d02a0a49cdd9d25d63f261b76df5ff52ccfa3eb
meteykaya <100938309+meteykaya@users.noreply.github.com>
2022-05-23 22:20:58 +0300

    Update bilgisayardaprogfinalproje.py

--- bilgisayardaprogfinalproje.py
+++ bilgisayardaprogfinalproje.py
@@ -19,3 +19,19 @@ class Ozankent():
     def plaka_tanıma(self): #Programın ana parçası. Araç plakalarının resim klasörü içindeki dosyaların okunarak sisteme aktarılmasıdır.
 
         klasor = "resim" #resim dosyalarının bulunduğu klasörü tanımlama
+
+        dosyalar=os.listdir(klasor) #klasör içindeki resimleri dosyalar değişkenine atama
+        for dosya in dosyalar: #dosyalar değişkeninin içindeki dosyaların sıra ile okunması
+                print("Site Bariyerlerine Gelen Araç :->",dosya) 
+
+                img = cv2.imread(dosya, cv2.IMREAD_COLOR)
+                img = cv2.resize(img, (600, 400))
+
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                gray = cv2.bilateralFilter(gray, 13, 15, 15)
+
+                edged = cv2.Canny(gray, 30, 200)
+                contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                contours = imutils.grab_contours(contours)
+                contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+                screenCnt = None
