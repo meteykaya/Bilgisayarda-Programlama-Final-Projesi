1f0441af0d31ad0941c9f8dc36489aeb096db967
meteykaya <100938309+meteykaya@users.noreply.github.com>
2022-05-23 22:01:18 +0300

    Update bilgisayardaprogfinalproje.py

--- bilgisayardaprogfinalproje.py
+++ bilgisayardaprogfinalproje.py
@@ -1,3 +1,21 @@
-print("Github Test")
-print("Degisiklikleri kaydetme Github Test")
-print("silinmis kodların islem gecmisinde gorulup gorulemedigini anlamak icin yazılmıstır")
+import sqlite3 #site veritabanı ile iletişim kurmak için kullanılmıştır.
+import cv2 # jpg dosyalarından plakaların okunarak sisteme aktarılması  için kullanılmıştır.
+import imutils
+import numpy as np
+import pytesseract
+import datetime # tarihlerin türkçe adlarıyla ifade edilmesi için
+import os #klasör değerlerinin alınması için kullanıldı
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Tesseract uzantıyı da tanımlamamız gerekiyor
+
+class Ozankent():
+    def __init__(self, isim):
+        self.veritabanı = sqlite3.connect("arac_plaka.db") #site veritabaı ile program arasında köprü
+        self.islem = self.veritabanı.cursor() # işlemlerin yapılabilmesi için cursor oluşturulması
+        self.isim = isim
+        self.durum = True # programın devamı için kontrol değeri atanması
+
+    def plaka_tanıma(self): #Programın ana parçası. Araç plakalarının resim klasörü içindeki dosyaların okunarak sisteme aktarılmasıdır.
+
+        klasor = "resim" #resim dosyalarının bulunduğu klasörü tanımlama
